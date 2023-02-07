6cbbb2f435c899f15fcae0244b3fe86af636a16b
Jessy Sztark <Jessy.Sztark@etu.univ-savoie.fr>
2023-02-07 14:46:08 +0100

    [1.1.0] New session & End of API (w\out Unit test)

--- SeriesAPI/Program.cs
+++ SeriesAPI/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<SeriesDbContext>(options =>
-  options.UseNpgsql(builder.Configuration.GetConnectionString("SeriesDbContext")));
+  options.UseNpgsql(builder.Configuration.GetConnectionString("DbSeries")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
